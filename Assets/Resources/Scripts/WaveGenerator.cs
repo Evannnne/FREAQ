bbc33f811ff69c68c86adaf899e62fcaf47df478
Evan Sarkozi <prefsuite@Live.ca>
2023-07-02 04:49:15 -0700

    got the rhythm challenge working after so long

--- Assets/Resources/Scripts/WaveGenerator.cs
+++ Assets/Resources/Scripts/WaveGenerator.cs
@@ -16,8 +16,8 @@ public class WaveGenerator : MonoBehaviour
 
     public float sweetSpotThreshold = 0.9f;
 
-    public float speed = 10f;
-    public float scale = 1f;
+    public AudioSource musicSource;
+    public float currentTime;
 
     private Texture2D m_texture;
     private Sprite m_sprite;
@@ -26,10 +26,11 @@ public class WaveGenerator : MonoBehaviour
 
     public float GetValueAt(float x)
     {
-        x *= scale;
-        x += Time.time * speed;
-
-        return Mathf.Sin(x);
+        float pos = x + musicSource.time * 2;
+        pos = Mathf.Max(0, pos);
+        int index = (int)(pos * frequency) % sampleBuffer.Length;
+        float sample = sampleBuffer[index];
+        return sample;
     }
 
 
@@ -39,6 +40,60 @@ public class WaveGenerator : MonoBehaviour
         m_texture.filterMode = FilterMode.Point;
         m_sprite = Sprite.Create(m_texture, new Rect(0, 0, m_texture.width, m_texture.height), Vector2.zero);
         m_colorBuffer = new Color[m_texture.width * m_texture.height];
+    }
+
+    private float[] sampleBuffer;
+    private float frequency;
+    private float max;
+    private void Start()
+    {
+        sampleBuffer = new float[musicSource.clip.samples];
+        musicSource.clip.LoadAudioData();
+        musicSource.clip.GetData(sampleBuffer, 0);
+
+        List<int> points = new List<int>();
+
+        // Find kicks/snares
+        int num0s = 0;
+        for(int i = 0; i < sampleBuffer.Length; i++)
+        {
+            if (sampleBuffer[i] <= 0.3f) num0s++;
+            else
+            {
+                if (num0s > 1500)
+                {
+                    points.Add(i);
+                }
+                num0s = 0;
+            }
+        }
+
+        frequency = musicSource.clip.frequency;
+
+        // Flush values
+        for (int i = 0; i < sampleBuffer.Length; i++)
+            sampleBuffer[i] = -1f;
+
+        // Create waves
+        foreach(int p in points)
+        {
+            float fac = 0.2f;
+            for(int j = 0; j < frequency * fac * 2; j++)
+            {
+                for(int s = -1; s <= 1; s += 2)
+                {
+                    float pt = (float)j / (frequency * fac);
+                    pt *= Mathf.PI / 2;
+                    int index = p + j * s;
+                    if (index >= 0 && index < sampleBuffer.Length)
+                    {
+
+                        sampleBuffer[index] = Mathf.Max(Mathf.Cos(pt), sampleBuffer[index]);
+                        sampleBuffer[index] = Mathf.Clamp(sampleBuffer[index], -1, 1);
+                    }
+                }
+            }
+        }
     }
 
     private void Update()
@@ -57,8 +112,9 @@ public class WaveGenerator : MonoBehaviour
                 Color targetColor;
 
                 float halfH = height / 2;
+                float halfW = width / 2;
                 float trueY = (y - halfH) / halfH * 2;
-                float trueX = (float)x / width;
+                float trueX = (x - halfW) / halfW * 2;
                 float atX = GetValueAt(trueX);
 
                 float diff = Mathf.Abs(trueY - atX);
@@ -72,8 +128,8 @@ public class WaveGenerator : MonoBehaviour
 
                 if (x < width / 2) targetColor.a *= 0.25f;
 
-                //if (targetColor != Color.white && Mathf.Abs(trueY - GetValueAt(0.5f)) < lineTolerance / 2f)
-                //    targetColor = new Color(0.5f, 0, 0, 1);
+                if (targetColor != Color.white && Mathf.Abs(trueY - GetValueAt(0)) < lineTolerance / 2f)
+                    targetColor = new Color(0.5f, 0, 0, 1);
 
                 m_colorBuffer[y * m_texture.width + x] = targetColor;
             }
@@ -88,7 +144,7 @@ public class WaveGenerator : MonoBehaviour
         {
             for (int s = -1; s <= 1; s += 2)
             {
-                if(GetValueAt(0.5f + checkInterval / checkSamples * i * s) >= sweetSpotThreshold)
+                if(GetValueAt(0 + checkInterval / checkSamples * i * s) >= sweetSpotThreshold)
                 {
                     sweetSpot = true;
                     break;
